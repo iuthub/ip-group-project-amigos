7b2264b70499c0f6eb11f52746b448591e2d0474
kelly <kjasurbekova@gmail.com>
2020-05-07 07:59:31 +0500

    changes to create category

--- resources/views/admin/category/allCategories.blade.php
+++ resources/views/admin/category/allCategories.blade.php
@@ -1,34 +1,26 @@
 @extends('layouts.master')
 @section('content')
 <div class="container">
-	<div class="form-group mt-4">
-		<a class="btnCategory" href="{{ route('createCategory') }}">Create category</a>
-	</div>
-	<hr>
-	<ul class="list-group">
-<<<<<<< HEAD
-	@foreach($category as $category)
-	<a class="categoryNameStyle" href="">
-		<li  class="list-group-item list-group-item-action">{{ $category->categoryName }}
-=======
-	@foreach($categories as $category)
-	<a href="{{ route('categoryMenu', ['id' => $category->id]) }}">
-		<li class="list-group-item list-group-item-action">{{ $category->categoryName }}
->>>>>>> 405a90799898595009ad8135334374d6d3addfec
-			<a class="float-right deleteBtn ml-4" href="{{ route('deleteCategory', ['id' => $category->id]) }}">
-				<i class="far fa-trash"></i>
-			</a>
-			<a class="float-right editBtn" href="{{ route('editCategory', ['id' => $category->id]) }}">
-				<i class="fas fa-edit"></i>
-			</a>
-		</li>
-	</a>
-	@endforeach
-	<div class="pagination-links">
-		{{ $categories->links() }}
-	</div>
+  <div class="form-group mt-4">
+    <a class="btnCategory" href="{{ route('createCategory') }}">Create category</a>
+  </div>
+  <hr>
+  <ul class="list-group">
+  @foreach($categories as $category)
+  <a  class="categoryNameStyle" href="{{ route('categoryMenu', ['id' => $category->id]) }}">
+    <li class="list-group-item list-group-item-action">{{ $category->categoryName }}
+      <a class="float-right deleteBtn ml-4" href="{{ route('deleteCategory', ['id' => $category->id]) }}">
+        <i class="far fa-trash"></i>
+      </a>
+      <a class="float-right editBtn" href="{{ route('editCategory', ['id' => $category->id]) }}">
+        <i class="fas fa-edit"></i>
+      </a>
+    </li>
+  </a>
+  @endforeach
+  <div class="pagination-links">
+    {{ $categories->links() }}
+  </div>
 </ul>
 </div>
-@endsection 
-
-
+@endsection
