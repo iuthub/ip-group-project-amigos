3e7d01a637abd3f9d6a70aa74740e98fde20cdbb
azimova86 <45287769+azimova86@users.noreply.github.com>
2020-05-09 14:02:29 +0500

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+<hr>
+<div class="subtitle">
+  <p class="restourant_name"><img class="logo_footer" src="{{ asset('logo/logo_footer.png') }}"></p>
+</div>
+<div>
+	<nav class="nav justify-content-center">
+		<a class="nav-link" href="#"><li class="icon-car"><img src="{{asset('images/icons/instagram.svg')}}"></li></a>
+		<a class="nav-link" href="#"><li class="icon-car"><img src="{{asset('images/icons/twitter.svg')}}"></li></a>
+		<a class="nav-link" href="#"><li class="icon-car"><img src="{{asset('images/icons/facebook.svg')}}"></li></a>
+	</nav>
+</div>
+<nav class="nav justify-content-center">
+	<a class="nav-link" href="{{ route('adminGetMenu') }}"><p class="black_text">FOOD & DRINK</p></a>
+	<a class="nav-link" href="{{ route('myOrders') }}"><p class="black_text">MAKE ORDER</p></a>
+	<a class="nav-link" href="/contact"><p class="black_text">CONTACT US</p></a>
+	@guest
+		<a class="nav-link" href="/login"><p class="black_text">SIGN IN</p></a>
+		<a class="nav-link" href="/register"><p class="black_text">REGISTER</p></a>
+	@endguest
+</nav>
